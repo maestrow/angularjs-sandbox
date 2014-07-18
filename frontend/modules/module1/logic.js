08867b907f8bbbd0fe6faa2b5631068b2ad273d7
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-07-19 00:40:21 +0400

    initial commit

---
+++ frontend/modules/module1/logic.js
@@ -0,0 +1,22 @@
+﻿var mod = angular.module('module1', ['ngResource']);
+
+mod.factory('Product', ['$resource', function ($resource) {
+
+	return $resource('/api/products/:prodId', {}, {
+		update: {
+			url: '/api/products/update/:id',
+			method: 'PUT',
+			params: { id: '@id' }
+		}
+	});
+}]);
+
+mod.controller('ProductList', [
+	'$scope', 'Product', function ($scope, Product) {
+		$scope.products = Product.query({ prodId: 1 });
+
+		$scope.action = function(actionName, params) {
+			new Product()['$' + actionName](urlParams, bodyParams);
+		};
+	}
+]);
